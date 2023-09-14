a721a5c54b71882b9c0c2ef04485e3d1e78a6ed6
Douglas Amorim <amorimdos@gmail.com>
2023-09-14 11:00:54 -0300

    feat crete code project

---
+++ Leetcode-solutions/Leetcode-solutions/Leetcode_solutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leetcode_solutionsApp.swift
+//  Leetcode-solutions
+//
+//  Created by Douglas Amorim on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Leetcode_solutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
